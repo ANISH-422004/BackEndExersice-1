8d7b6fee21e9627764a9d8919ee77404fe5cd30f
Kazi Mohibuzzaman <kazimohib51@gmail.com>
2025-01-20 16:50:22 +0530

    create user and form handling done

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -1,17 +1,37 @@
-const userModel = require("../models/userModel")
+const userModel = require("../models/userModel");
 
 module.exports.homeController = async (req, res) => {
+  try {
+    const users = await userModel.find();
+    res.render("index", { users });
+  } catch (error) {
+    res.render("error", { error });
+  }
+};
 
-        try {
-                const users = await userModel.find()
-                res.render("index", { users })
+module.exports.createController = (req, res) => {
+  res.render("create");
+};
 
-        } catch (error) {
-                res.render("error", { error })
-
-        }
+module.exports.formController = async (req, res) => {
+  console.log(req.body);
 
 
-        
+  try {
+    const { username, profileImage, email, bio, gender, age } = req.body;
 
-}
+    const newUserData = new userModel({
+      username: username,
+      profileImage: profileImage,
+      email: email,
+      bio: bio,
+      gender: gender,
+      age: age,
+    });
+
+    await newUserData.save();
+    res.redirect("/");
+  } catch (error) {
+    res.render("error", { error });
+  }
+};
--- src/routes/index.route.js
+++ src/routes/index.route.js
@@ -3,5 +3,7 @@ const router =express.Router()
 const controller = require("../controllers/index.controller")
 
 router.get("/" , controller.homeController)
+router.get("/create", controller.createController)
+router.post("/create", controller.formController)
 
 module.exports = router; 
