73789767d20e00927c51fd6a69694129bda5c585
ANISH-422004 <anishbhattacharya422004@gmail.com>
2025-01-21 14:55:40 +0530

    EditfunctionDone

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -1,4 +1,7 @@
 const userModel = require("../models/userModel");
+const mongoose = require("mongoose");
+const ObjectId = mongoose.Types.ObjectId;
+
 
 module.exports.homeController = async (req, res) => {
   try {
@@ -16,17 +19,16 @@ module.exports.createController = (req, res) => {
 module.exports.formController = async (req, res) => {
   console.log(req.body);
 
-
   try {
     const { username, profileImage, email, bio, gender, age } = req.body;
 
     const newUserData = new userModel({
-      username: username,
-      profileImage: profileImage,
-      email: email,
-      bio: bio,
-      gender: gender,
-      age: age,
+      username,
+      profileImage,
+      email,
+      bio,
+      gender,
+      age,
     });
 
     await newUserData.save();
@@ -36,26 +38,77 @@ module.exports.formController = async (req, res) => {
   }
 };
 
-module.exports.showEachController = async(req, res) => {
-        console.log()        
-        try {
-                const requestedUserEmail = req.params.useremail
-                const user =  await userModel.findOne({email : requestedUserEmail})
-                res.render("show",{user})
-        } catch (error) {
-                res.render("error", { error });
-        }
-};
-
-module.exports.deleteController = async(req, res) => {
-  console.log()        
+module.exports.showEachController = async (req, res) => {
   try {
-          const requestedUserName = req.params.username 
-          const user =  await userModel.findOneAndDelete({
-            username: requestedUserName})
+    const requestedUserEmail = req.params.useremail;
+    const user = await userModel.findOne({ email: requestedUserEmail });
 
-          res.redirect("/")
+    if (!user) {
+      return res.render("error", { error: "User not found" });
+    }
+
+    res.render("show", { user });
   } catch (error) {
-          res.render("error", { error });
+    res.render("error", { error });
   }
 };
+
+module.exports.deleteController = async (req, res) => {
+  try {
+    const requestedUserName = req.params.username;
+    const user = await userModel.findOneAndDelete({ username: requestedUserName });
+
+    if (!user) {
+      return res.render("error", { error: "User not found" });
+    }
+
+    res.redirect("/");
+  } catch (error) {
+    res.render("error", { error });
+  }
+};
+
+module.exports.showeditController = async (req, res) => {
+  try {
+    const dbId = req.params.dbId;
+    // console.log(req.params);
+
+    // Validate if dbId is a valid MongoDB ObjectId
+    if (!ObjectId.isValid(dbId)) {
+      return res.render("error", { error: "Invalid User ID" });
+    }
+
+
+    const user = await userModel.findOne({ _id: new ObjectId(dbId) });
+
+    if (!user) {
+      return res.render("error", { error: "User not found" });
+    }
+
+    res.render("edit", { user });
+  } catch (error) {
+    res.render("error", { error });
+  }
+};
+
+
+
+module.exports.editController = async (req, res) => {
+  try {
+    const dbId = req.params.dbId;
+    if (!ObjectId.isValid(dbId)) {
+      return res.render("error", { error: "Invalid User ID" });
+    }
+    const userforUpdate = await userModel.findOne({ _id: new ObjectId(dbId) });
+
+    
+    
+    const { username, profileImage, email, bio, gender, age } = req.body;
+
+    await userModel.findOneAndUpdate(userforUpdate  , { username, profileImage, email, bio, gender, age } )
+    res.redirect("/")
+
+  } catch (error) {
+    res.render("error", { error });
+  }
+}
--- src/routes/index.route.js
+++ src/routes/index.route.js
@@ -7,4 +7,9 @@ router.get("/create", controller.createController)
 router.post("/create", controller.formController)
 router.get("/show/:useremail", controller.showEachController)
 router.get("/delete/:username", controller.deleteController)
+router.get("/edit/:dbId", controller.showeditController)
+router.post("/update/:dbId", controller.editController)
+
+
+
 module.exports = router;   
